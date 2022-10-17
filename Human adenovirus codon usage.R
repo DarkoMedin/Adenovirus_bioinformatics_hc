d579904c8cd101a2f8a829a80f35be4929c66d0f
Darko Medin <71905959+DarkoMedin@users.noreply.github.com>
2022-10-17 12:56:40 +0200

    updated path line
    the update of the line to load the data

--- Human adenovirus codon usage.R
+++ Human adenovirus codon usage.R
@@ -5,7 +5,7 @@ library(ggplot2)
 library(pheatmap)
 
 #Import the dataset
-codon_usage <- read.csv("C:/Users/DARKO/Desktop/Projects/Codon usage taxa project/codon_usage.csv")
+codon_usage <- read.csv("path/codon_usage.csv")
 View(codon_usage)
 
 #Make the data object adenodat which will be used for data wrangling
